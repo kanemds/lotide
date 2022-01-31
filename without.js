b129f620ae902df280110c5dd8d8b55a422cd3fd
Kanem <kanemdeng@gmail.com>
2022-01-31 04:43:14 +0000

    without.js added

---
+++ without.js
@@ -0,0 +1,19 @@
+// const without = (setOne,setTwo) => {
+//   const result = setOne.filter((item) => {
+//     return setTwo.indexOf(item) < 0;
+//   });
+//   return result;
+// };
+const without = (setOne,setTwo) => {
+  let result = [];
+  for (let i = 0; i < setOne.length; i++) {
+    if (setTwo.indexOf(setOne[i]) < 0) {
+      result.push(setOne[i]);
+    }
+  }
+  return result;
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+console.log(without(["1", "2", "3"], [1, 2, "3", "6", 9 , "v", "1","1"]));
