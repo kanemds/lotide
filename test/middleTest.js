42fbc096e72aa608c72b0c63729f167327885629
Kanem <kanemdeng@email.com>
2022-02-18 15:06:26 -0800

    update

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,29 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+
+
+const assert = require('chai').assert;
 const middle = require('../middle');
-console.log(middle([1,2]));
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);// => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-console.log(middle([1, 2, 3, 4, 5, 6]));
-console.log(middle([1, 2, 3, 4, 5, 6,"a","c","4","1","4","3","q"]));
+
+// Note that for comparing arrays, Chai gives us assert.deepEqual.
+
+// because assert.strictEqual simply uses === to compare values, which will not work for comparing objects and arrays.
+
+describe("middle", ()=> {
+  it("return []", ()=> {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  it("return [3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("return [2, 3]", ()=> {
+    assert.deepEqual(middle([1, 2, 3, 4]),[2, 3]);
+  });
+  it("return [3, 4]", ()=> {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+  });
+  
+});
+
+
+
+
