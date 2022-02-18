70f1f7af9869c964df9e501e748ca6f17e376c3a
Kanem <kanemdeng@email.com>
2022-02-17 18:48:02 -0800

    head.js update

--- head.js
+++ head.js
@@ -10,8 +10,5 @@ const head = (data) => {
   }
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 8);
 
 module.exports = head;
