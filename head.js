465a0e598624551ae9dad40970c44cc55a0f790f
Kanem <kanemdeng@email.com>
2022-02-17 18:42:42 -0800

    head.js update

--- head.js
+++ head.js
@@ -1,18 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑😂 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  if (actual === expected) {
-    console.log(`✅😁 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("aaa", "bbb");
-assertEqual(1, "2");
-assertEqual("A","A");
 
 const head = (data) => {
   if (!data) {
@@ -27,4 +14,4 @@ assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5,6,7]), 8);
 
-
+module.exports = head;
