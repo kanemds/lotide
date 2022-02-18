8858b38d3314d3d556b6997672cdffcf3efcc8d3
Kanem <kanemdeng@email.com>
2022-02-17 19:18:52 -0800

    update

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑😂 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  if (actual === expected) {
-    console.log(`✅😁 Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("aaa", "bbb");
-assertEqual(1, "2");
-assertEqual("A","A");
-
+const assertEqual = require('./assertEqual');
 
 
 const eqArrays = (firstSet,secondSet) => {
@@ -29,11 +14,5 @@ const eqArrays = (firstSet,secondSet) => {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+module.exports = eqArrays;
 
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
