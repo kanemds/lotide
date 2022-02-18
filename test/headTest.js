b57c632201be478cdd53447b1b0cba68f67b8f6a
Kanem <kanemdeng@email.com>
2022-02-18 14:56:21 -0800

    update

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,9 @@
 // const assertEqual = require('../assertEqual');
 
 
+// Note that for comparing arrays, Chai gives us assert.deepEqual.
+
+// because assert.strictEqual simply uses === to compare values, which will not work for comparing objects and arrays.
 
 
 
