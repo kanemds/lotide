d150209c3408005b36645beb2816daf440ef6d17
Kanem <kanemdeng@email.com>
2022-02-17 19:53:52 -0800

    update

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,4 @@
-const eqArrays = (firstSet,secondSet) => {
-  
-  if (firstSet.length !== secondSet.length) {
-    return false;
-  }
-  for (let i = 0; i < firstSet.length; i++) {
-    if ((firstSet[i] !== secondSet[i])) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (a,b) => {
   if (eqArrays(a,b) === false) {
@@ -23,6 +7,5 @@ const assertArraysEqual = (a,b) => {
     console.log("😉😉 It's equal!");
   }
 };
-assertArraysEqual([1,2,3,3,4,5],["1","2",4,5]);
-assertArraysEqual([1,2,3,],[1,2,3]);
-assertArraysEqual([1,2,3,],[1,2,"3"]);
+
+module.exports = assertArraysEqual;
