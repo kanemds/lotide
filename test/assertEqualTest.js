d607148edf6c0f4721a24e2b69e441ed639c027e
Kanem <kanemdeng@email.com>
2022-02-17 19:02:57 -0800

    update

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -12,3 +12,14 @@ const head = require('../head.js');
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5,6,7]), 8);
+
+const tail = require('../tail.js');
+const letter = ["a"];
+const empty = [];
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+// no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(tail(words));
+console.log(tail(letter));
+console.log(tail(empty));
