d40e4fa7a93756a616bb34ae64b2c8dca4d95980
Kanem <kanemdeng@email.com>
2022-02-18 18:20:03 -0800

    update

--- letterPositions.js
+++ letterPositions.js
@@ -1,18 +1,22 @@
-const conutLetters = (sentence) => {
+const letterPositions = (sentence) => {
   let results = {};
   for (let i = 0; i < sentence.length; i ++) {
-    const letter = sentence[i];
+    const letter = sentence[i].toLowerCase();
     if (letter === " ") {
       continue;
       // take out empty space
     }
     if (results[letter]) {
+      //when there is same letter, since, its array, we can push the index.
       results[letter].push(i);
     } else {
+      //this one make the key became array
       results[letter] = [i];
     }
   }
   return results;
 };
 
-console.log(conutLetters("lighthouse in the house"));
+console.log(letterPositions("lighthouse in the house"));
+
+module.exports = letterPositions;
